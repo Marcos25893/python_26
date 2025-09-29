cbe271bb301b6fa23360a1bff4f7da0d59cd77b1
Marcos <mromjim783@g.educaand.es>
2025-09-29 19:07:48 +0200

    Python

---
+++ condicional.py
@@ -0,0 +1,11 @@
+password = "123456789"
+
+password_enc = "123456789"
+
+if password == password_enc:
+    print("Acceso concedido")
+    print("otra cosa")
+else:
+    print("Acceso denegado")
+
+print("Fin del programa")
